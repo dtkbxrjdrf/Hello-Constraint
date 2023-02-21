ed0efacd58d1c76cb15d6af42207d8e562f343b7
Serhii Yatsenko <dtkbxrjdrf@gmail.com>
2023-02-21 16:16:01 +0200

    Finish commit

---
+++ app/src/main/java/it_school/sumdu/edu/hellotoast/implicit_activity.java
@@ -0,0 +1,14 @@
+package it_school.sumdu.edu.hellotoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class implicit_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_implicit);
+    }
+}
